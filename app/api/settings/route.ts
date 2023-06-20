363a8cad06b75cde89a8c98ae72641b483e3cc1b
Mohammad Poormehdi <mohammad.poormehdi1383@gmail.com>
2023-06-20 16:51:15 +0330

    setting modal created
    setting modal with the functionality of updating users created

---
+++ app/api/settings/route.ts
@@ -0,0 +1,22 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import { NextResponse } from "next/server";
+import prisma from "@/libs/prismadb";
+
+export async function POST(request: Request) {
+  try {
+    const currentUser = await getCurrentUser();
+    const body = await request.json();
+    const { name, image } = body;
+    if (!currentUser?.id) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const updatedUser = await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { image, name },
+    });
+    return NextResponse.json(updatedUser);
+  } catch (error) {
+    console.error(error, "ERROR_SETTINGS_MESSAGE");
+    return new NextResponse("internal Error", { status: 500 });
+  }
+}
